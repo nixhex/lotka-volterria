da295bcbc64abe56b5a1a9ac434500f428f16f74
nixhex <fevinster@gmail.com>
2025-10-25 10:47:44 -0700

    moved spritesheet to header, added images to .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,21 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <iostream>
+#include "assets/spritesheet_png.hpp"
 
 constexpr int sheep_row         = 21;
-constexpr int sheep_right_col   = 0;
+constexpr int sheep_col         = 0;
 constexpr int sprite_width      = 16;
 constexpr int sprite_height     = 16;
 
-constexpr sf::Vector2i SHEEP_POSE_1{
-    sheep_right_col*sprite_width,
+constexpr sf::Vector2i SHEEP_POSE_1 {
+    sheep_col*sprite_width,
     sheep_row*sprite_height
 };
 
 constexpr int wolf_row =  24;
-constexpr int wolf_col =   0;
-constexpr sf::Vector2i WOLF_POSE_1{
+constexpr int wolf_col =   8;
+constexpr sf::Vector2i WOLF_POSE_1 {
     wolf_col*sprite_width,
     wolf_row*sprite_height
 };
@@ -25,14 +26,20 @@ int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Lotka-Volterria");
     window.setFramerateLimit(144);
-    //sf::Texture sheep("src/sprites/spritesheet.png", false, sf::IntRect({0,22*16}, {16,16}));
     try {
-        //sf::Texture sheep("src/sprites/sheep_white.png");
-        sf::Texture sheep("src/sprites/spritesheet.png", false, 
-            sf::IntRect(SHEEP_POSE_1, SPRITE_DIMS));
+        sf::Texture sheep, wolf;
+
+        // load sprites from src_sprites_spritesheet_png,
+        // generated w/ cmd: xxd -i src\sprites\spritesheet.png 
+        // | Out-File -Encoding ascii -NoNewline src\assets\spritesheet_png.hpp 
+        if(!sheep.loadFromMemory(src_sprites_spritesheet_png,
+            src_sprites_spritesheet_png_len, false,
+            sf::IntRect(SHEEP_POSE_1, SPRITE_DIMS))) return 1;
         sf::Sprite sheepSprite(sheep);
-        sf::Texture wolf("src/sprites/spritesheet.png", false,
-            sf::IntRect(WOLF_POSE_1, SPRITE_DIMS));
+
+        if(!wolf.loadFromMemory(src_sprites_spritesheet_png,
+            src_sprites_spritesheet_png_len, false,
+            sf::IntRect(WOLF_POSE_1, SPRITE_DIMS))) return 1;
         sf::Sprite wolfSprite(wolf);
 
         sheepSprite.setOrigin({sprite_width/2, sprite_height/2});
